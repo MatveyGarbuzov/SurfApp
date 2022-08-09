659de7fc1039d090a8206f0be58be3fcf3ee3062
MatveyGarbuzov <matveyvoronezh@mail.ru>
2022-08-09 19:27:08 +0300

    init-app

---
+++ SurfApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SurfApp
+//
+//  Created by Matvey Garbuzov on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
